2238856d3007fcab9a05bd4c7ba63d90684cf64a
PeeNo1zaZ12300 <86155546+PeeNo1zaZ12300@users.noreply.github.com>
2021-08-05 16:13:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
   Admins: ["UserID", "UserID"], //Admins of the bot
   ExpressServer: true,//If you wanted to make the website run or not
-  DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
+  DefaultPrefix: process.env.Prefix || "=>", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
-  Token: process.env.Token || "", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
+  Token: process.env.Token || "ODcyNzY4MDgzMDkyMTc2OTM2.YQuqng.67a5Wg4TODbYr31d5iHpb25w9fo", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "872768083092176936", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "xwxlGfnYnj3DKDL-xtjBsnWZ_i40N_U9", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
@@ -36,7 +36,7 @@ module.exports = {
 
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "", //Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "", //Spotify Client Secret
+    ClientID: process.env.Spotify_ClientID || "42e79a85f40f4cc584590f29c8da1015", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "b74356d32b0d4a499e22645eb0a0beeb", //Spotify Client Secret
   },
 };
